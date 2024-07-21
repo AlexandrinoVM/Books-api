450f97890d1b4a04693e5e7efed946e540957dbb
alexandrinoVM <vinicius.moraes2@outlook.com>
2024-07-21 11:11:11 -0300

    delete and put controler and create the routes

--- src/controllers/booksController.js
+++ src/controllers/booksController.js
@@ -1,9 +1,59 @@
-const getBooks =('/books',(req,res)=>{
-    const {name,author,launch } = req.params.body  
-    res.json({message: 'List of itens'})
-})
+const booksMethods = require('../models/booksModel')
 
+const getBooks = async(req,res) =>{
+    try{
+        const books = await booksMethods.getBooks()
+        res.send(200).json(books)
+    }
+    catch (error){
+        res.status(500).json({message: 'erro trying getting book'})
+    }
+}
 
-const postBooks =('/books',(req,res)=>{
-    
-}) 
+const getBooksId = async(req,res) =>{
+    try{
+        const books = await booksMethods.getBooksByid(req.body.id)
+        res.send(200).json(books)
+    }
+    catch (error){
+        res.status(500).json({message: 'erro trying getting book'})
+    }
+}
+
+const putBooks = async(req,res) =>{
+    try{
+        const books = await booksMethods.updateBook(req.body.id)
+        res.send(200).json(books)
+    }
+    catch (error){
+        res.sen.status(500).json({message: 'error trying to update the book'})
+    }
+}
+
+const deleteBooks = async(req,res) =>{
+    try{
+        const books = await booksMethods.deleteBook(req.body.id);
+        res.send(200).json(books)
+
+    }catch (error){
+        res.status(500).json({message: 'error trying delete book'})
+    }
+}
+
+const postBooks = async(req,res) =>{
+    try{
+        const books = await booksMethods.createeBook();
+        res.send(200).json(books)
+    }
+    catch (error){
+        res.status(500).json({message: 'error trying to create book'})
+    }
+} 
+
+module.exports ={
+    getBooks,
+    putBooks,
+    postBooks,
+    getBooksId,
+    deleteBooks
+}
---
+++ src/routes/booksRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const booksController = require('../controllers/booksController')
+
+router.get('/books',booksController.getBooks)
+router.get('/books/:id',booksController.getBooksId)
+router.put('/books/:id',booksController.putBooks)
+router.get('/books/:id',booksController.deleteBooks)
+router.post('/books',booksController.postBooks)
+
+
+module.exports = router
